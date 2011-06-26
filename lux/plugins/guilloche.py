7498a2ea8d9f66e8415a3cab7e785a5adc74d8a7
fenn <fennetic.net>
2011-06-26 09:56:33 -0700

    improve color handling, clean up cruft

--- lux/plugins/guilloche.py
+++ lux/plugins/guilloche.py
@@ -12,35 +12,48 @@ import math
 class SimplePlugin(LuxPlugin):
     name = "Guilloche"
     description = """
-    A spirograph-like pattern
+    A spirograph-like pattern.
+    Set crn_dwell and crv_dwell to 0!
     """
     def __init__(self):
         lux.register(Parameter( name = "simple_rate",
                                 description = "0..1   controls the rate of spinning cubes",
                                 default_value = 1.0 ))
-        #eventually i'll port these to lux parameters, but right now there doesn't seem to be any reason to
-        self.max_segments = 400 #tweak according to openlase calibration parameters; too high can cause ol to crash
-        self.max_cycles = 15 # set high (~50) for maximum glitch factor
-        self.time = 0
+        #eventually i'll port these to lux parameters, but right now there doesn't seem to be any reason to since there are no GUI sliders
+        self.max_segments = 300 #tweak according to openlase calibration parameters; too high can cause ol to crash
+        self.max_cycles = 10 # set high (~50) for maximum glitch factor
+        self.time = 1
         self.time_step = 1/30
         self.time_scale = 1/1
         self.theta_step = 0.01
         self.R = 0.25 # big steps
-        self.R_period = 1/100
-        self.r = 0.08 # little stepsi
-        self.r_period = 1/37
-        self.p = 0.5 # size of the ringa
-        self.p_period = 1/6
-        self.scale = 5
+        self.R_frequency = 1/100
+        self.r = 0.08 # little steps
+        self.r_frequency = 1/37
+        self.p = 0.5 # size of the ring
+        self.p_frequency = 1/2000
+        self.color_time_frequency = 1/4
+        self.color_length_frequency = 1/240 #set to 0 to calibrate color
+        self.color_angle_frequency = 1
+        self.r_prime = 1 #37
+        self.g_prime = 2 #23
+        self.b_prime = 3 #128
+        
+        self.scale = 3
         self.width = self.scale
         self.height = self.scale
-        self.bass = 0 # plz hack this to do fft kthx
+        self.bass = 1 # plz hack this to do fft power binning kthx
+
     def draw(self):
         time = lux.time
+        time = self.time
+        ctf = self.color_time_frequency
+        clf = self.color_length_frequency
+        caf = self.color_angle_frequency
         theta = math.sin(time*self.time_scale)
-        R = self.R * math.sin(time*self.time_scale*self.R_period)
-        r = self.r * math.sin(time*self.time_scale*self.r_period)
-        p = self.p * math.sin(time*self.time_scale*self.p_period) * self.bass
+        R = self.R * math.sin(2*pi*time*self.time_scale*self.R_frequency)
+        r = self.r * math.sin(2*pi*time*self.time_scale*self.r_frequency)
+        p = self.p * math.sin(2*pi*time*self.time_scale*self.p_frequency) * self.bass
 
         ol.color3(1.0, 0.0, 1.0);
         ol.loadIdentity3()
@@ -59,9 +72,17 @@ class SimplePlugin(LuxPlugin):
             y *= self.height
             if first:
                 ol.begin(ol.LINESTRIP)
-                ol.vertex3((x,y,0))
                 first = False
-            ol.color3(math.sin(time*n/37), math.cos(time*n/23), math.tan(time*n/128))
+            #red = math.sin(ctf*time*n/37) * math.sin(csf*theta*n/37)
+            #green = math.sin(ctf*time*n/23) * math.sin(csf*theta*n/23)
+            #blue = math.sin(ctf*time*n/128) * math.sin(csf*theta*n/128)
+            
+            angle = math.atan2(y, x)/(2*pi)
+            red   = math.sin(2*pi*(self.r_prime/3+ctf*time+clf*n+caf*angle))
+            green = math.sin(2*pi*(self.g_prime/3+ctf*time+clf*n+caf*angle))
+            blue =  math.sin(2*pi*(self.b_prime/3+ctf*time+clf*n+caf*angle))
+
+            ol.color3(red, green, blue)
             ol.vertex3((x,y,0))
             n += 1
         ol.end()
@@ -69,23 +90,6 @@ class SimplePlugin(LuxPlugin):
         target = self.max_segments * 0.8
         error = (target - n)/target
         self.theta_step = min(max(1e-100, self.theta_step * (1-error)), 1)
-        print n
-        #self.time += self.time_step
-#        self.theta_step = self.theta_step -0.0000003
-        #self.theta_step = self.theta_step -0.00003
+        print n, time
+        self.time += 1/30
 
-       
-        #test square
-        if False:
-            ol.begin(ol.LINESTRIP)
-            ol.vertex3((0,1,0))
-            ol.vertex3((1,1,0))
-            ol.vertex3((1,0,0))
-            ol.vertex3((0,0,0))
-            ol.end()
-        
-        if False:            
-            ol.scale3((0.6, 0.6, 0.6))
-            ol.rotate3Z(lux.time * pi * 0.1 * lux.simple_rate)
-            ol.rotate3X(lux.time * pi * 0.8 * lux.simple_rate)
-            ol.rotate3Y(lux.time * pi * 0.73 * lux.simple_rate)
