64a9f3ebb9f5f93fee0cb63ebfdb19217992b310
Michael Broxton <broxton@gmail.com>
2011-06-27 01:06:12 -0700

    fix flicker, make color prettier wrt actual lasers

--- lux/plugins/guilloche.py
+++ lux/plugins/guilloche.py
@@ -20,30 +20,30 @@ class SimplePlugin(LuxPlugin):
                                 description = "0..1   controls the rate of spinning cubes",
                                 default_value = 1.0 ))
         #eventually i'll port these to lux parameters, but right now there doesn't seem to be any reason to since there are no GUI sliders
-        self.max_segments = 300 #tweak according to openlase calibration parameters; too high can cause ol to crash
-        self.max_cycles = 10 # set high (~50) for maximum glitch factor
+        self.max_segments = 600 #tweak according to openlase calibration parameters; too high can cause ol to crash
+        self.max_cycles = 5 # set high (~50) for maximum glitch factor
         self.time = 1
         self.time_step = 1/30
-        self.time_scale = 1/1
+        self.time_scale = 1
         self.theta_step = 0.01
         self.R = 0.25 # big steps
-        self.R_frequency = 1/100
+        self.R_frequency =  1/100
         self.r = 0.08 # little steps
-        self.r_frequency = 1/37
+        self.r_frequency = 1/370
         self.p = 0.5 # size of the ring
         self.p_frequency = 1/2000
-        self.color_time_frequency = 1/4
-        self.color_length_frequency = 1/240 #set to 0 to calibrate color
-        self.color_angle_frequency = 1
-        self.spatial_resonance = 4 #ok why is this 5 and not 4?
+        self.color_time_frequency = 1/10
+        self.color_length_frequency = 0 #3/240 #set to 0 to calibrate color
+        self.color_angle_frequency = 0.5
+        self.spatial_resonance = 3 #ok why is this 5 and not 4?
         self.spatial_resonance_amplitude = 0.1 
         self.spatial_resonance_offset = 0.25
         
-        self.r_prime = 1 #37
-        self.g_prime = 2 #23
-        self.b_prime = 3 #128
+        self.r_prime = 3 #37 
+        self.g_prime = 2 #23 
+        self.b_prime = 1 #128 
         
-        self.scale = 3
+        self.scale = 1
         self.width = self.scale
         self.height = self.scale
         self.bass = 1 # plz hack this to do fft power binning kthx
@@ -51,10 +51,10 @@ class SimplePlugin(LuxPlugin):
 
     def draw(self):
         time = lux.time
-        time = self.time
+        #time = self.time
         ctf = self.color_time_frequency
         clf = self.color_length_frequency
-        caf = self.color_angle_frequency
+        caf = self.color_angle_frequency/2
         theta = abs(math.sin(time*self.time_scale))
         R = self.R * math.sin(2*pi*time*self.time_scale*self.R_frequency)
         r = self.r * math.sin(2*pi*time*self.time_scale*self.r_frequency)
@@ -76,16 +76,17 @@ class SimplePlugin(LuxPlugin):
             y = (R + r) * math.sin(theta) + (r + p) * math.sin((R+r)/r * theta)
             
             if first:
-                ol.begin(ol.LINESTRIP)
+                #ol.begin(ol.LINESTRIP)
+                ol.begin(ol.POINTS)
                 first = False
             #red = math.sin(ctf*time*n/37) * math.sin(csf*theta*n/37)
             #green = math.sin(ctf*time*n/23) * math.sin(csf*theta*n/23)
             #blue = math.sin(ctf*time*n/128) * math.sin(csf*theta*n/128)
             
             angle = math.atan2(y, x)/(2*pi)
-            red   = math.sin(2*pi*(self.r_prime/3+ctf*time+clf*n+caf*angle))
-            green = math.sin(2*pi*(self.g_prime/3+ctf*time+clf*n+caf*angle))
-            blue =  math.sin(2*pi*(self.b_prime/3+ctf*time+clf*n+caf*angle))
+            red   = abs(math.sin(2*pi*(self.r_prime/3+ctf*time+clf*n+caf*angle)))
+            green = abs(math.sin(2*pi*(self.g_prime/3+ctf*time+clf*n+caf*angle)))
+            blue =  abs(math.sin(2*pi*(self.b_prime/3+ctf*time+clf*n+caf*angle)))
             ol.color3(red, green, blue)
             
             #this makes it square-ish
