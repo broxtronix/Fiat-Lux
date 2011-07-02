fb107d58830c50c4c870627434e46c6e19e11708
fenn <fennetic.net>
2011-07-02 13:10:01 -0700

    works mostly

--- lux/plugins/reach.py
+++ lux/plugins/reach.py
@@ -7,6 +7,7 @@
  *
  Ported from processing (http://processing.org/) examples.
 """
+from __future__ import division
 from lux_plugin import LuxPlugin
 from parameters import lux, Parameter
 from audio import audio_engine
@@ -16,38 +17,36 @@ import math
 from math import pi
 import random
 
-SEGMENT_LENGTH = 0.025
+#SEGMENT_LENGTH = 0.025
+arm_length = 1.5
+swoosh_frequency = 1/5
+parts = 20
 
 class Segment():
     def __init__(self, x, y, width):
         self.x, self.y = x, y
         self.width = width
+        self.previous = None
 
     def draw(self):
-        ol.translate3(self.x, self.y, 0)
-        ol.rotate3Z(self.angle)
         ol.begin(ol.LINESTRIP)
-        ol.vertex3((0,0,0))
-        ol.vertex3((SEGMENT_LENGTH, 0, 0))
+        try:
+            ol.vertex3((self.prev_x,self.prev_y,0))
+        except AttributeError:
+            pass
+        ol.vertex3((self.x, self.y, 0))
         ol.end()
-
-        #self.graphics.move_to(0, 0)
-        #self.graphics.line_to(SEGMENT_LENGTH, 0)
-        #self.graphics.set_color("#999")
-        #self.graphics.set_line_style(width = width)
-        #self.graphics.stroke()
-
 
 class Arm():
     def __init__(self):
+        self.x, self.y = -1, -1
         self.segments = []
-        parts = 20
         for i in range(parts):
             segment = Segment(0, 0, i)
             self.segments.append(segment)
 
     def draw(self, target):
-        x, y =target[0], target[1]
+        x, y =target[0]-self.x, target[1]-self.y
 
         def get_angle(segment, x, y):
             return math.atan2(dy, dx)
@@ -60,14 +59,20 @@ class Arm():
             segment.angle = angle
             segment.rotation = angle
 
-            x = x - math.cos(angle) * SEGMENT_LENGTH
-            y = y - math.sin(angle) * SEGMENT_LENGTH
+            x = x - math.cos(angle) * arm_length/parts
+            y = y - math.sin(angle) * arm_length/parts
 
         # and now move the pointed nodes, starting from the last one
         # (that is the beginning of the arm)
         for prev, segment in reversed(list(zip(self.segments, self.segments[1:]))):
-            prev.x = segment.x + math.cos(segment.angle) * SEGMENT_LENGTH
-            prev.y = segment.y + math.sin(segment.angle) * SEGMENT_LENGTH
+            prev.x = segment.x + math.cos(segment.angle) * arm_length/parts
+            prev.y = segment.y + math.sin(segment.angle) * arm_length/parts
+            segment.prev_x = prev.x
+            segment.prev_y = prev.y
+
+        ol.translate3((self.x, self.y, 0))
+        for segment in self.segments:
+            segment.draw()
 
     def on_enter_frame(self, scene, context):
         self.segments[-1].y = self.height
@@ -92,8 +97,9 @@ class SimplePlugin(LuxPlugin):
         ol.translate3((0, 0, -3))
 
         #target = [random.uniform(-1, 1), random.uniform(-1, 1)]
-        target = [math.sin(2*pi*lux.time*3), math.sin(2*pi*lux.time*5)]
-        self.arm.draw(target)
+        #target_x, target_y = random.uniform(-1, 1), random.uniform(-1, 1)
+        target_x, target_y = math.sin(2*pi*swoosh_frequency*lux.time*3), math.sin(2*pi*swoosh_frequency*lux.time*5)
+        self.arm.draw([target_x, target_y])
 
         ol.begin(ol.LINESTRIP)
         ol.vertex3((-1,  1,  1))
